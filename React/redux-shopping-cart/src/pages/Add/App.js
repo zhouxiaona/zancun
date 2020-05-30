af605a5295da6861295235200d1e024f5438e1f0
邓博 <dengbp@laoyuegou.com>
2020-05-30 19:16:27 +0800

    remove sw

--- React/redux-shopping-cart/src/pages/Add/App.js
+++ React/redux-shopping-cart/src/pages/Add/App.js
@@ -3,13 +3,65 @@ import AddTodo from './AddTodo'
 import Footer from './Footer'
 import TodoList from './TodoList'
 
-const App = () => {
-    return (
-        <div>
-            <AddTodo/>
-            <TodoList/>
-            <Footer/>
-        </div>
-    )
-}
+// const App = () => {
+//     return (
+//         <div>
+//             <AddTodo/>
+//             <TodoList/>
+//             <Footer/>
+//         </div>
+//     )
+// }
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = { val: 0 };
+    }
+    
+    componentDidMount() {
+      this.setState({val: this.state.val + 1});
+      console.log(this.state.val,'1');    // 第 1 次 log  =》0
+  
+      this.setState({val: this.state.val + 1});
+      console.log(this.state.val,'2');    // 第 2 次 log =》0
+
+    //   this.setState({val:this.state.val+1},()=>{
+    //     console.log(this.state.val,'3');
+    //   })
+    //   console.log(this.state.val,'4');
+      setTimeout(() => {
+        this.setState({val: this.state.val + 1});
+        console.log(this.state.val,'3');  // 第 3 次 log 2
+  
+        this.setState({val: this.state.val + 1});
+        console.log(this.state.val,'4');  // 第 4 次 log 3
+      }, 0);
+
+    // 请用React程序把两个数组 ['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2'] 
+    // ['A', 'B', 'C', 'D']，合并为 ['A1', 'A2', 'A', 'B1', 'B2', 'B', 'C1', 'C2', 'C', 'D1', 'D2', 'D']
+      let arr1=['A1', 'A2', 'B1', 'B2', 'C1', 'C2', 'D1', 'D2'] 
+      let arr2=['A', 'B', 'C', 'D']
+      let arr3=[]
+      for(let i=0;i<arr1.length;i++){
+        arr3.push(arr1[i])
+        if((i+1)%2===0){
+            arr3.push(arr2[((i+1)-2)/2])
+        }
+      }
+    //   console.log(arr3)
+
+    // var arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
+    // // 编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组
+    // console.log(Array.from(new Set(arr.flat(Infinity))).sort((a,b)=>a-b))
+
+
+
+    }
+  
+    render() {
+        console.log('render==')
+      return null;
+    }
+  };
+  
 export default App
--- React/redux-shopping-cart/src/pages/Count/Hook.js
+++ React/redux-shopping-cart/src/pages/Count/Hook.js
@@ -2,128 +2,128 @@ import React from 'react'
 // import 'react-count-animation/dist/count.min.css';
 // import AnimationCount from 'react-count-animation';
 
-// export default function Hook() {
-//       // 声明一个新的叫做 “count” 的 state 变量
-//       const [countObj, setCount] = useState({count: 0})
-//       useEffect(()=>{
-//         document.title=`clicked ${countObj.count} times`
-//       })
+export default function Hook() {
+      // 声明一个新的叫做 “count” 的 state 变量
+      const [countObj, setCount] = useState({count: 0})
+      useEffect(()=>{
+        document.title=`clicked ${countObj.count} times`
+      })
       
-//       const [isOnLine, setIsOnLine] = useState(null)
-//       useEffect(() => {
-//         // function handleStatusChange(status) {
-//         //   setIsOnline(status.isOnline)
-//         // }
-//         // ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange)
-//         // return function cleanup() {
-//         //   ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange)
-//         // }
-//       })
-//       const settings = {
-//         start: 99923,
-//         count: 9999999,
-//         duration: 3000,
-//         decimals: 4,
-//         useGroup: true,
-//         animation: 'up',
-//       };
-//       const settings2 = {
-//         start: 1,
-//         count: 999999,
-//         duration: 4000,
-//         decimals: 2,
-//         useGroup: true,
-//         animation: 'roll',
-//       };
-//       const settings3 = {
-//         start: 1,
-//         count: 9999999,
-//         duration: 3000,
-//         decimals: 2,
-//         useGroup: true,
-//         animation: 'slide',
-//       };
+      const [isOnLine, setIsOnLine] = useState(null)
+      useEffect(() => {
+        // function handleStatusChange(status) {
+        //   setIsOnline(status.isOnline)
+        // }
+        // ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange)
+        // return function cleanup() {
+        //   ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange)
+        // }
+      })
+      const settings = {
+        start: 99923,
+        count: 9999999,
+        duration: 3000,
+        decimals: 4,
+        useGroup: true,
+        animation: 'up',
+      };
+      const settings2 = {
+        start: 1,
+        count: 999999,
+        duration: 4000,
+        decimals: 2,
+        useGroup: true,
+        animation: 'roll',
+      };
+      const settings3 = {
+        start: 1,
+        count: 9999999,
+        duration: 3000,
+        decimals: 2,
+        useGroup: true,
+        animation: 'slide',
+      };
 
-//       return (
-//         <div>
-//           <p>You clicked {countObj.count} times</p>
-//           <button onClick={() => setCount({count: countObj.count + 1})}> Click me </button>
-//           <div onClick={()=> setIsOnLine(!isOnLine)}>{isOnLine ? 'OnLine' : 'OffLine'}</div>
-//           <div>
-//             <h1 className="title">Count Animation</h1>
-//             <div className="exam-div">
-//               <AnimationCount {...settings}/>
-//             </div>
-//             <h1 className="title">Count Roll</h1>
-//             <div className="exam-div">
-//               <AnimationCount {...settings2}/>
-//             </div>
-//             <h1 className="title">Count Slide</h1>
-//             <div className="exam-div">
-//               <AnimationCount {...settings3} />
-//             </div>
-//           </div>
-//         </div>
-//       )
-//     }
-
-    export default class Hook extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          count: 0
-        };
-      }
-
-      componentWillMount(){
-        console.log('componentWillMount==')
-      }
-    
-      componentDidMount() {
-        console.log('componentDidMount==')
-        document.title = `You clicked ${this.state.count} times`;
-        setTimeout(()=>{
-          this.setState({
-            count:7
-          })
-        },1000)
-      }
-
-      componentWillUnmount(){
-        console.log('componentWillUnmount')
-      }
-
-      componentWillReceiveProps(){
-        console.log('componentWillReceiveProps')
-      }
-
-      componentWillUpdate(){
-        console.log('componentWillUpdate')
-      }
-      
-      componentDidCatch(){
-        console.log('componentDidCatch')
-      }
-    
-      componentDidUpdate() {
-        console.log('componentDidUpdate==')
-        document.title = `You clicked ${this.state.count} times`;
-      }
-
-      shouldComponentUpdate(nextState,nextProps){
-        console.log('shouldComponentUpdate==',nextState,nextProps)
-        return true
-      }
-    
-      render() {
-        console.log('render==')
-        return (
+      return (
+        <div>
+          <p>You clicked {countObj.count} times</p>
+          <button onClick={() => setCount({count: countObj.count + 1})}> Click me </button>
+          <div onClick={()=> setIsOnLine(!isOnLine)}>{isOnLine ? 'OnLine' : 'OffLine'}</div>
           <div>
-            <p>You clicked {this.state.count} times</p>
-            <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-              Click me
-            </button>
+            <h1 className="title">Count Animation</h1>
+            <div className="exam-div">
+              <AnimationCount {...settings}/>
+            </div>
+            <h1 className="title">Count Roll</h1>
+            <div className="exam-div">
+              <AnimationCount {...settings2}/>
+            </div>
+            <h1 className="title">Count Slide</h1>
+            <div className="exam-div">
+              <AnimationCount {...settings3} />
+            </div>
           </div>
-        );
-      }
+        </div>
+      )
     }
+
+    // export default class Hook extends React.Component {
+    //   constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       count: 0
+    //     };
+    //   }
+
+    //   componentWillMount(){
+    //     console.log('componentWillMount==')
+    //   }
+    
+    //   componentDidMount() {
+    //     console.log('componentDidMount==')
+    //     document.title = `You clicked ${this.state.count} times`;
+    //     setTimeout(()=>{
+    //       this.setState({
+    //         count:7
+    //       })
+    //     },1000)
+    //   }
+
+    //   componentWillUnmount(){
+    //     console.log('componentWillUnmount')
+    //   }
+
+    //   componentWillReceiveProps(){
+    //     console.log('componentWillReceiveProps')
+    //   }
+
+    //   componentWillUpdate(){
+    //     console.log('componentWillUpdate')
+    //   }
+      
+    //   componentDidCatch(){
+    //     console.log('componentDidCatch')
+    //   }
+    
+    //   componentDidUpdate() {
+    //     console.log('componentDidUpdate==')
+    //     document.title = `You clicked ${this.state.count} times`;
+    //   }
+
+    //   shouldComponentUpdate(nextState,nextProps){
+    //     console.log('shouldComponentUpdate==',nextState,nextProps)
+    //     return true
+    //   }
+    
+    //   render() {
+    //     console.log('render==')
+    //     return (
+    //       <div>
+    //         <p>You clicked {this.state.count} times</p>
+    //         <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+    //           Click me
+    //         </button>
+    //       </div>
+    //     );
+    //   }
+    // }
